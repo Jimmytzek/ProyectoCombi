a01fe71acf8f196e1ffc3e23dedde8c775effea7
CoxFelipe <37556245+CoxFelipe@users.noreply.github.com>
2019-06-13 22:47:20 -0500

    v3

---
+++ geodrive/site_media/html/combi_reporte.php
@@ -0,0 +1,30 @@
+<?php
+require('mysql_table.php');
+
+class PDF extends PDF_MySQL_Table
+{
+function Header()
+{
+	// Title
+	$this->SetFont('Arial','',18);
+	$this->Cell(0,6,'Combis',0,1,'C');
+	$this->Ln(10);
+	// Ensure table header is printed
+	parent::Header();
+}
+}
+
+// Connect to database
+$link = mysqli_connect('localhost','root','','geodrive');
+
+$pdf = new PDF();
+$pdf->AddPage();
+// First table: output all columns
+$pdf->Table($link,'SELECT * FROM `combi`');
+
+$prop = array('HeaderColor'=>array(255,150,100),
+			'color1'=>array(210,245,255),
+			'color2'=>array(255,255,210),
+			'padding'=>2);
+$pdf->Output();
+?>
